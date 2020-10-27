ea2ba2afa424ac1b6ae5cbb9b8a9dffd69ce0cd3
Caleb Harmon <calebmharmon@gmail.com>
2020-10-26 22:51:51 -0500

    added menu functionality and more skins

---
+++ Assets/Scripts/GlobalVariablesScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariablesScript : MonoBehaviour
+{
+    public static GlobalVariablesScript Instance;
+    public int test;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/SelectionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Skin[] skins;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class Skin
+{
+    public Sprite jumping;
+    public Sprite still;
+}
